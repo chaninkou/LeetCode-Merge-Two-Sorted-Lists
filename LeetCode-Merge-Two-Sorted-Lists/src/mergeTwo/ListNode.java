6182dbb90234f578bfc76d7aa7045bbf74c33e76
chaninkou <michaelkoulove@yahoo.com>
2019-06-09 21:24:52 -0700

    Finished another coding problem.

---
+++ LeetCode-Merge-Two-Sorted-Lists/src/mergeTwo/ListNode.java
@@ -0,0 +1,10 @@
+package mergeTwo;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
---
+++ LeetCode-Merge-Two-Sorted-Lists/src/mergeTwo/Main.java
@@ -0,0 +1,33 @@
+package mergeTwo;
+
+
+public class Main {
+	public static void main(String[] args){
+		ListNode l1 = new ListNode(1);
+		l1.next = new ListNode(2);
+		l1.next.next = new ListNode(4);
+		
+		ListNode l2 = new ListNode(1);
+		l2.next = new ListNode(3);
+		l2.next.next = new ListNode(4);
+		
+		MergeTwoSortedListsSolution list = new MergeTwoSortedListsSolution();
+		
+		ListNode node = list.mergeTwoLists(l1, l2);
+		
+		display(node);
+	}
+	
+	// Displaying the linked list
+	public static void display(ListNode head){
+		if(head == null){
+			return;
+		}
+		ListNode current = head;
+		while(current != null){
+			System.out.print(current.val + " -> ");
+			current = current.next;
+		}
+		System.out.println(current);
+	}
+}
